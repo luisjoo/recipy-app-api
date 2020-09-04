c22f9ab3d07fbfbb012492d9149c4f667544d3f2
Luis Ortíz Martínez <luisjoseortiz.ortiz@gmail.com>
2020-09-03 20:43:57 -0600

    Updated admin so it can be accessible from the web

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,26 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+class AdminSiteTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@luis.ortiz',
+            password='test123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@luis.ortiz',
+            password='test123',
+            name='Test user full name'
+        )
+
+    def test_user_listed(self):
+        """Test that the users are listed on user page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
